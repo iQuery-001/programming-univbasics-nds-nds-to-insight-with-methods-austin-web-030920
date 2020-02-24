267d4b4055b2018a4291d738088e1d09ccec63c5
Dakota Willingham <kota@vaga.tk>
2020-02-24 22:36:17 +0000

    Done.

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -5,9 +5,9 @@ require 'directors_database'
 # using director_data as input
 def gross_for_director(director_data)
   director_gross = 0
-  while movie_counter < director_data[director_counter][:movies].length do
+  while movie_counter < director_data[:movies].length do
     movie_counter = 0
-    director_gross += director_data[director_counter][:movies][:worldwide_grosses]
+    director_gross += director_data[:movies][:worldwide_grosses]
     movie_counter += 1
   end
   director_gross
@@ -21,7 +21,7 @@ def directors_totals(nds)
   result = {}
   director_counter = 0
   while director_counter < nds.length do
-    result[nds[director_counter][:name]] = gross_for_director(nds)
+    result[nds[director_counter][:name]] = gross_for_director(nds[director_counter])
     director_counter += 1
   end
   result
