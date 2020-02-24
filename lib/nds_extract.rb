54e43e51982bb6472d313aa044b0cf002f675829
Dakota Willingham <kota@vaga.tk>
2020-02-24 22:40:27 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -10,6 +10,7 @@ def gross_for_director(director_data)
     director_gross += director_data[:movies][:worldwide_grosses]
     movie_counter += 1
   end
+  puts director_gross
   director_gross
 end
 
